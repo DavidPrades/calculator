5a79eefc4694d8e997e505442e87f790b19ad49f
alu20908719v <alu20908719v@ti2-09>
2018-03-15 19:27:47 +0100

    Added backspace behavior

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -345,7 +345,12 @@ public class MainFrame extends javax.swing.JFrame {
     }//GEN-LAST:event_btnEqualActionPerformed
 
     private void btnBackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBackActionPerformed
-        // TODO add your handling code here:
+        String s=textFieldDisplay.getText();
+        
+        if(s.length()>0){
+        String subs= s.substring(0, s.length() -1);
+        textFieldDisplay.setText(subs);
+        }
     }//GEN-LAST:event_btnBackActionPerformed
 
     /**
